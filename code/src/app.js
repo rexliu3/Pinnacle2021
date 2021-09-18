cf17517a9d768116eb71170b506db45c0105f8c6
Rex Liu <rexliu33@gmail.com>
2021-09-18 15:17:57 -0500

    icon for origin

--- code/src/app.js
+++ code/src/app.js
@@ -160,13 +160,57 @@ async function initMap() {
   map.addListener("bounds_changed", () => {
     searchBoxOrigin.setBounds(map.getBounds());
   });
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBoxOrigin.addListener("places_changed", () => {
+    const places = searchBoxOrigin.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
 
   const inputDestination = document.getElementById("destination");
   const searchBoxDestination = new google.maps.places.SearchBox(inputDestination);
   map.addListener("bounds_changed", () => {
     searchBoxDestination.setBounds(map.getBounds());
   });
-
   return map;
 }
 
