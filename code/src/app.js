7e7a2568ac63957ca2571864779dd36b3bb66cc4
Anton Chen <contact@antonchen.ca>
2021-09-19 01:05:25 -0700

    Use new crime hotspot optimization alg selection

--- code/src/app.js
+++ code/src/app.js
@@ -638,14 +638,39 @@ async function getAvoidAreaString(start, end) {
   };
 
   // Return distance of point pt from fastest path.
-  let pathDistanceFromCenter = (pt) => {
-    let center = {
-      lat: (start.latitude + end.latitude) / 2,
-      lng: (start.longitude + end.longitude) / 2
-    };
-    let dx = Math.abs(center.lat - pt.lat);
-    let dy = Math.abs(center.lng - pt.lng);
-    return dx * dx + dy * dy;
+  // Source: wikipedia LMAO
+  let pathDistanceFromOptimal = (pt) => {
+    let x0 = pt.lat, y0 = pt.lng;
+    let x1 = start.latitude, y1 = start.longitude;
+    let x2 = end.latitude, y2 = end.longitude;
+
+    var A = x0 - x1;
+    var B = y0 - y1;
+    var C = x2 - x1;
+    var D = y2 - y1;
+
+    var dot = A * C + B * D;
+    var len_sq = C * C + D * D;
+    var param = -1;
+    if (len_sq != 0) // in case of 0 length line
+        param = dot / len_sq;
+
+    var xx, yy;
+
+    if (param < 0) {
+      xx = x1;
+      yy = y1;
+    } else if (param > 1) {
+      xx = x2;
+      yy = y2;
+    } else {
+      xx = x1 + param * C;
+      yy = y1 + param * D;
+    }
+
+    var dx = x - xx;
+    var dy = y - yy;
+    return Math.sqrt(dx * dx + dy * dy);
   };
 
   let res = "";
@@ -664,7 +689,7 @@ async function getAvoidAreaString(start, end) {
       pts.push(pt);
     } else {
       for (let i = 0; i < pts.length; i++) {
-        if (pathDistanceFromCenter(pt) < pathDistanceFromCenter(pts[i])) {
+        if (pathDistanceFromOptimal(pt) < pathDistanceFromOptimal(pts[i])) {
           pts.splice(i, 0, pt);
           break;
         }
